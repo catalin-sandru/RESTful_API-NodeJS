d224cf1ab5ddeb2cc7569f304a9eaa35c2d8fc55
Catalin Sandru <catalin.sandru88@gmail.com>
2019-06-07 13:10:40 +0100

    added edit and delete methods

--- api/routes/products.js
+++ api/routes/products.js
@@ -19,12 +19,24 @@ router.get('./:productId', (req, res, next) => {
     res.status(200).json({
       message: 'You discovered the special ID',
       id: id
-    })
+    });
   } else {
     res.status(200).json({
       message: 'You passes an ID'
-    })
+    });
   }
-})
+});
+
+router.patch('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Updated product!'
+  });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Deleted product!'
+  });
+});
 
 module.exports = router;
