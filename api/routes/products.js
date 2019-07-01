76e0cac74bcc6eda5015b94bd39ef1217d16e095
Catalin Sandru <catalin.sandru88@gmail.com>
2019-07-01 12:10:10 +0100

    upload file with multer

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer')
-const upload = multer({dest: 'uploads/'})
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({storage: storage})
 
 const Product = require('../models/product')
 
